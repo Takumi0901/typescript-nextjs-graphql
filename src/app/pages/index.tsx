de56bc7fec27ce1f6ffe9e2fe13c439d32ff19dd
takumi kimura <juventus0901@gmail.com>
2018-11-22 23:42:48 +0900

    Added mutations

--- src/app/pages/index.tsx
+++ src/app/pages/index.tsx
@@ -1,5 +1,6 @@
+import * as React from 'react'
 import gql from 'graphql-tag'
-import { Query } from 'react-apollo'
+import { Query, Mutation } from 'react-apollo'
 import withData from '../config'
 
 import AuthorList from './AuthorList'
@@ -13,21 +14,52 @@ const query = gql`
   }
 `
 
+const ADD_AUTHOR = gql`
+  mutation insert_author($objects: [author_insert_input!]) {
+    insert_author(objects: $objects) {
+      affected_rows
+      returning {
+        id
+        name
+      }
+    }
+  }
+`
 const Index = () => {
+  let input: any
   return (
-    <Query // <- Wrapping the main component with Query component from react-apollo
-      query={query}
-      fetchPolicy={'cache-and-network'}
-    >
-      {({ data: { author: authors } }) => {
-        return (
+    <React.Fragment>
+      <Query query={query} fetchPolicy={'cache-and-network'}>
+        {({ data: { author: authors } }) => {
+          return (
+            <div>
+              <h1>My Authors </h1>
+              <AuthorList authors={authors} />
+            </div>
+          )
+        }}
+      </Query>
+      <Mutation mutation={ADD_AUTHOR}>
+        {(addAuthor, { data }) => (
           <div>
-            <h1>My Authors </h1>
-            <AuthorList authors={authors} />
+            <form
+              onSubmit={e => {
+                e.preventDefault()
+                addAuthor({ variables: { objects: [{ name: input.value }] } })
+                input.value = ''
+              }}
+            >
+              <input
+                ref={node => {
+                  input = node
+                }}
+              />
+              <button type="submit">Add Author</button>
+            </form>
           </div>
-        )
-      }}
-    </Query>
+        )}
+      </Mutation>
+    </React.Fragment>
   )
 }
 
